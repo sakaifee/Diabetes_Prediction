b8c29e3efce905395f275bff52377198482e8c29
sakaifee <shadanalamkaifee@gmail.com>
2025-08-02 01:56:48 +0530

    Updated code

--- src/data_loader.py
+++ src/data_loader.py
@@ -14,11 +14,6 @@ def split_data(df, test_size=0.2, random_state=42):
 if __name__ == "__main__":
     df = load_data()
     X_train, X_test, y_train, y_test = split_data(df)
-
-    # print(f"[INFO] X_train shape: {X_train.shape}")
-    # print(f"[INFO] X_test shape: {X_test.shape}")
-    # print(f"[INFO] y_train distribution:\n{y_train.value_counts()}")
-
     print(f"X_train shape: {X_train.shape}")
     print(f"X_test shape: {X_test.shape}")
     print(f"y_train distribution: {y_train.value_counts()}")
